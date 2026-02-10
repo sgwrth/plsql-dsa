d46a403392411f360900fc42b5d666505441b2a0
Andreas Siegwarth <andreassiegwarth@startmail.com>
2026-02-10 20:40:12 +0100

    * change: renamed list.sql to linkedlist.sql * add: traversing the linked list

--- src/linkedlist.sql
+++ src/linkedlist.sql
@@ -15,18 +15,27 @@ DECLARE
 	g_nodes t_storage;
 	g_head PLS_INTEGER := NULL;
 	g_free PLS_INTEGER := 0;
+	v_index PLS_INTEGER;
 BEGIN
-	DBMS_OUTPUT.PUT_LINE('hello world');
-
 	FOR i IN 1..10
 	LOOP
-		g_free := g_free + i;
+		g_free := g_free + 1;
 		g_nodes(g_free).id := g_free;
 		g_nodes(g_free).value := 100 + (i * 10);
-		g_nodes(g_free).next := g_free;
+		g_nodes(g_free).next := g_head;
+		g_head := g_free;
+		DBMS_OUTPUT.PUT_LINE('g_head: ' || g_head);
 	END LOOP;
 
 	DBMS_OUTPUT.PUT_LINE(g_nodes.COUNT);
+
+	v_index := g_head;
+
+	WHILE g_nodes.EXISTS(v_index)
+	LOOP
+		DBMS_OUTPUT.PUT_LINE(g_nodes(v_index).value);
+		v_index := g_nodes(v_index).next;
+	END LOOP;
 
 END;
 /
