403201af8b62f03d69667f9c4f2c1e3c594661b7
Andreas Siegwarth <andreassiegwarth@startmail.com>
2026-02-12 21:20:38 +0100

    * change: in bubblesort, moved sort procedure to package

--- src/bubblesort.sql
+++ src/bubblesort.sql
@@ -1,19 +1,39 @@
 CREATE OR REPLACE TYPE t_number_list AS TABLE OF NUMBER;
 /
-CREATE OR REPLACE PACKAGE bubblesort
+
+CREATE OR REPLACE PACKAGE bubblesort_pkg
 IS
 	PROCEDURE print_values(numbers IN t_number_list);
+	PROCEDURE sort(numbers IN OUT t_number_list);
 END;
 /
 
-CREATE OR REPLACE PACKAGE BODY bubblesort
+CREATE OR REPLACE PACKAGE BODY bubblesort_pkg
 IS
 	PROCEDURE print_values(numbers IN t_number_list)
 	AS
 	BEGIN
 		FOR i IN 1..numbers.COUNT
 		LOOP
-			dbms_output.put_line(numbers(i));
+			DBMS_OUTPUT.PUT_LINE(numbers(i));
+		END LOOP;
+	END;
+
+	PROCEDURE sort(numbers IN OUT t_number_list)
+	AS
+		temp PLS_INTEGER;
+	BEGIN
+		FOR i IN 1..numbers.COUNT
+		LOOP
+			FOR j IN 1..(numbers.COUNT - 1)
+			LOOP
+				IF numbers(j) > numbers(j + 1)
+				THEN
+					temp := numbers(j);
+					numbers(j) := numbers(j + 1);
+					numbers(j + 1) := temp;
+				END IF;
+			END LOOP;
 		END LOOP;
 	END;
 END;
@@ -21,25 +41,13 @@ END;
 
 DECLARE
 	v_numbers t_number_list;
-	v_temp PLS_INTEGER;
 BEGIN
 	SELECT DISTINCT salary BULK COLLECT INTO v_numbers FROM hr.employees;
 
-	bubblesort.print_values(v_numbers);
+	bubblesort_pkg.print_values(v_numbers);
 
-	FOR i IN 1..v_numbers.COUNT
-	LOOP
-		FOR j IN 1..(v_numbers.COUNT - 1)
-		LOOP
-			IF v_numbers(j) > v_numbers(j + 1)
-			THEN
-				v_temp := v_numbers(j);
-				v_numbers(j) := v_numbers(j + 1);
-				v_numbers(j + 1) := v_temp;
-			END IF;
-		END LOOP;
-	END LOOP;
+	bubblesort_pkg.sort(v_numbers);
 
-	bubblesort.print_values(v_numbers);
+	bubblesort_pkg.print_values(v_numbers);
 END;
 /
