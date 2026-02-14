9eb896a242dd78f6ef38cadb25f4f5380781718e
Andreas Siegwarth <andreassiegwarth@startmail.com>
2026-02-14 09:09:33 +0100

    * add: for binarytreesearch, types t_node, node_table, and init_tree function

--- src/binarytreesearch.sql
+++ src/binarytreesearch.sql
@@ -1,11 +1,36 @@
+CREATE OR REPLACE TYPE t_node
+IS
+OBJECT
+(
+	key		NUMBER,
+	value		NUMBER,
+	left_node	NUMBER,
+	right_node	NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE node_table
+IS
+TABLE OF t_node;
+/
+
 CREATE OR REPLACE PACKAGE binary_tree_search_pkg
 IS
+	FUNCTION init_tree RETURN node_table;
 	PROCEDURE insert_node;
 END;
 /
 
 CREATE OR REPLACE PACKAGE BODY binary_tree_search_pkg
 IS
+	FUNCTION init_tree RETURN node_table
+	IS
+		tree node_table;
+	BEGIN
+		tree := node_table();
+		return tree;
+	END;
+
 	PROCEDURE insert_node
 	IS
 	BEGIN
@@ -15,7 +40,10 @@ END;
 /
 
 DECLARE
+	tree node_table;
 BEGIN
-	binary_tree_search_pkg.insert_node();
+	tree := binary_tree_search_pkg.init_tree;
+	DBMS_OUTPUT.PUT_LINE('tree size: ' || tree.COUNT);
+	binary_tree_search_pkg.insert_node;
 END;
 /
