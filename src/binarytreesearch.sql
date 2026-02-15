f0516c7c1418e8aa46677e08032cb6b216bb4f94
Andreas Siegwarth <andreassiegwarth@startmail.com>
2026-02-15 07:53:14 +0100

    * add: for binarytreesearch, working implementation of insert_node() function

--- src/binarytreesearch.sql
+++ src/binarytreesearch.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TYPE t_node
 IS
 OBJECT
 (
-	key		NUMBER,
+	id		NUMBER,
 	value		NUMBER,
 	left_node	NUMBER,
 	right_node	NUMBER
@@ -17,7 +17,8 @@ TABLE OF t_node;
 CREATE OR REPLACE PACKAGE binary_tree_search_pkg
 IS
 	FUNCTION init_tree RETURN node_table;
-	PROCEDURE insert_node;
+	PROCEDURE insert_node(tree IN OUT node_table, value IN NUMBER);
+	PROCEDURE print_size(tree IN node_table);
 END;
 /
 
@@ -31,10 +32,55 @@ IS
 		return tree;
 	END;
 
-	PROCEDURE insert_node
+
+	PROCEDURE insert_node(tree IN OUT node_table, value IN NUMBER)
+	IS
+		new_node t_node;
+		target_node_id NUMBER;
+		found_node BOOLEAN := FALSE;
+		node_inserted BOOLEAN := FALSE;
+		counter PLS_INTEGER := 0;
+	BEGIN
+		new_node := t_node(tree.COUNT + 1, value, NULL, NULL);
+		IF tree.COUNT = 0 THEN
+			tree.EXTEND;
+			tree(tree.LAST) := new_node;
+			DBMS_OUTPUT.PUT_LINE('inserted as root node');
+		ELSE
+			target_node_id := tree.FIRST;
+			WHILE found_node = FALSE AND target_node_id IS NOT NULL LOOP
+				IF value < tree(target_node_id).value THEN
+					IF tree(target_node_id).left_node IS NULL THEN
+						found_node := TRUE;
+						tree.EXTEND;
+						tree(tree.LAST) := new_node;
+						tree(target_node_id).left_node := new_node.id;
+						node_inserted := TRUE;
+					ELSE
+						target_node_id := tree(target_node_id).left_node;
+					END IF;
+				ELSIF value > tree(target_node_id).value THEN
+					IF tree(target_node_id).right_node IS NULL THEN
+						found_node := TRUE;
+						tree.EXTEND;
+						tree(tree.LAST) := new_node;
+						tree(target_node_id).right_node := new_node.id;
+						node_inserted := TRUE;
+					ELSE
+						target_node_id := tree(target_node_id).right_node;
+					END IF;
+				ELSE
+					EXIT;
+				END IF;
+			END LOOP;
+		END IF;
+	END;
+
+
+	PROCEDURE print_size(tree IN node_table)
 	IS
 	BEGIN
-		DBMS_OUTPUT.PUT_LINE('inserting');
+		DBMS_OUTPUT.PUT_LINE('tree size: ' || tree.COUNT);
 	END;
 END;
 /
@@ -43,7 +89,9 @@ DECLARE
 	tree node_table;
 BEGIN
 	tree := binary_tree_search_pkg.init_tree;
-	DBMS_OUTPUT.PUT_LINE('tree size: ' || tree.COUNT);
-	binary_tree_search_pkg.insert_node;
+	binary_tree_search_pkg.print_size(tree);
+	binary_tree_search_pkg.insert_node(tree, 42);
+	binary_tree_search_pkg.insert_node(tree, 404);
+	binary_tree_search_pkg.print_size(tree);
 END;
 /
