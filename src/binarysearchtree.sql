c9e816d5202ab273091d5f117d8a349d2bef3030
Andreas Siegwarth <andreassiegwarth@startmail.com>
2026-02-15 16:20:55 +0100

    * change: in binarysearchtree, extracted some code to find_and_insert_node() procedure * change: in binarysearchtree, some refactoring

--- src/binarysearchtree.sql
+++ src/binarysearchtree.sql
@@ -17,13 +17,21 @@ CREATE OR REPLACE PACKAGE bst_pkg
 IS
 	FUNCTION init_tree RETURN node_table;
 	PROCEDURE insert_node(tree IN OUT node_table, value IN NUMBER);
+	PROCEDURE find_and_insert_node
+	(
+		side		IN	VARCHAR2,
+		found_node	IN OUT	BOOLEAN,
+		tree		IN OUT	node_table,
+		crnt_nodeid	IN OUT	NUMBER,
+		value		IN	NUMBER
+	);
 	PROCEDURE insert_left_or_right
 	(
 		side		IN	VARCHAR2,
 		found_node	IN OUT	BOOLEAN,
 		tree		IN OUT	node_table,
-		current_node_id	IN	INTEGER,
-		new_node	IN	t_node
+		crnt_nodeid	IN	INTEGER,
+		value		IN	NUMBER
 	);
 	PROCEDURE print_size(tree IN node_table);
 END;
@@ -43,55 +51,65 @@ IS
 	PROCEDURE insert_node(tree IN OUT node_table, value IN NUMBER)
 	IS
 		new_node	t_node;
-		current_node_id	INTEGER;
+		crnt_nodeid	INTEGER;
 		found_node	BOOLEAN := FALSE;
 	BEGIN
-		new_node := t_node(value, NULL, NULL);
 		IF tree.COUNT = 0 THEN
 			tree.EXTEND;
-			tree(tree.LAST) := new_node;
-		ELSE
-			current_node_id := tree.FIRST;
-			WHILE found_node = FALSE AND current_node_id IS NOT NULL LOOP
-				IF value < tree(current_node_id).value THEN
-					IF tree(current_node_id).left IS NULL THEN
-						insert_left_or_right('l', found_node, tree,
-								     current_node_id, new_node);
-					ELSE
-						current_node_id := tree(current_node_id).left;
-					END IF;
-				ELSIF value > tree(current_node_id).value THEN
-					IF tree(current_node_id).right IS NULL THEN
-						insert_left_or_right('r', found_node, tree,
-								     current_node_id, new_node);
-					ELSE
-						current_node_id := tree(current_node_id).right;
-					END IF;
-				ELSE
-					EXIT;
-				END IF;
-			END LOOP;
+			tree(tree.LAST) := t_node(value, NULL, NULL);
+			RETURN;
 		END IF;
+		crnt_nodeid := tree.FIRST;
+		WHILE found_node = FALSE AND crnt_nodeid IS NOT NULL LOOP
+			IF value < tree(crnt_nodeid).value THEN
+				find_and_insert_node('l', found_node, tree,
+						     crnt_nodeid, value);
+			ELSIF value > tree(crnt_nodeid).value THEN
+				find_and_insert_node('r', found_node, tree,
+						     crnt_nodeid, value);
+			ELSE
+				RETURN;
+			END IF;
+		END LOOP;
 	END;
 
+
+	PROCEDURE find_and_insert_node
+	(
+		side		IN	VARCHAR2,
+		found_node	IN OUT	BOOLEAN,
+		tree		IN OUT	node_table,
+		crnt_nodeid	IN OUT	NUMBER,
+		value		IN	NUMBER
+	)
+	IS
+	BEGIN
+		IF tree(crnt_nodeid).left IS NULL THEN
+			insert_left_or_right(side, found_node, tree,
+					     crnt_nodeid, value);
+		ELSE
+			crnt_nodeid := tree(crnt_nodeid).left;
+		END IF;
+	END;
+	
 
 	PROCEDURE insert_left_or_right
 	(
 		side		IN	VARCHAR2,
 		found_node	IN OUT	BOOLEAN,
 		tree		IN OUT	node_table,
-		current_node_id	IN	INTEGER,
-		new_node	IN	t_node
+		crnt_nodeid	IN	INTEGER,
+		value		IN	NUMBER
 	)
 	IS
 	BEGIN
 		found_node := TRUE;
 		tree.EXTEND;
-		tree(tree.LAST) := new_node;
+		tree(tree.LAST) := t_node(value, NULL, NULL);
 		IF side = 'l' THEN
-			tree(current_node_id).left := tree.LAST;
+			tree(crnt_nodeid).left := tree.LAST;
 		ELSIF side = 'r' THEN
-			tree(current_node_id).right := tree.LAST;
+			tree(crnt_nodeid).right := tree.LAST;
 		END IF;
 	END;
 
