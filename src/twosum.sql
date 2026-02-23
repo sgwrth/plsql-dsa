829c0cfa05c23a8b81fed28b559c206a99a52937
Andreas Siegwarth <andreassiegwarth@startmail.com>
2026-02-23 20:56:28 +0100

    * add: for twosum, implement swap(), partition() and (quick)sort()

--- src/twosum.sql
+++ src/twosum.sql
@@ -5,36 +5,41 @@ CREATE OR REPLACE PACKAGE twosum
 IS
 	TYPE t_nums IS TABLE OF INTEGER;
 
-	FUNCTION has_two_sum(p_nums IN t_nums, p_sum IN INTEGER) RETURN BOOLEAN;
+	FUNCTION has_twosum(p_nums IN OUT t_nums, p_sum IN INTEGER) RETURN BOOLEAN;
+
 
 	PROCEDURE sort
 	(
-		p_nums	IN t_nums,
-		p_begin	IN INTEGER,
-		p_end	IN INTEGER
+		p_nums	IN OUT	t_nums,
+		p_begin	IN	INTEGER,
+		p_end	IN	INTEGER
 	);
 
-	PROCEDURE partition
+
+	FUNCTION partition
 	(
-		p_nums	IN t_nums,
-		p_begin	IN INTEGER,
-		p_end	IN INTEGER
-	);
+		p_nums	IN OUT	t_nums,
+		p_begin	IN	INTEGER,
+		p_end	IN	INTEGER
+	) RETURN INTEGER;
+
 
 	PROCEDURE swap
 	(
-		p_nums	IN OUT t_nums,
-		p_val_a	IN INTEGER,
-		p_val_b	IN INTEGER
+		p_nums	IN OUT	t_nums,
+		p_idx_a	IN	INTEGER,
+		p_idx_b	IN	INTEGER
 	);
+
 
 	FUNCTION find_twosum_element
 	(
-		p_nums	IN t_nums,
-		p_sum	IN INTEGER
+		p_nums	IN OUT	t_nums,
+		p_sum	IN	INTEGER
 	) RETURN INTEGER;
 
 	FUNCTION are_equal(p_nums_a IN t_nums, p_nums_b IN t_nums) RETURN BOOLEAN;
+
 
 	FUNCTION find
 	(
@@ -43,58 +48,131 @@ IS
 		p_end	IN INTEGER,
 		p_value	IN INTEGER
 	) RETURN INTEGER;
+
+
+	PROCEDURE print(nums IN t_nums);
 END;
 /
 
 CREATE OR REPLACE PACKAGE BODY twosum
 IS
-	FUNCTION has_two_sum(p_nums IN t_nums, p_sum IN INTEGER) RETURN BOOLEAN
+	FUNCTION has_twosum(p_nums IN OUT t_nums, p_sum IN INTEGER) RETURN BOOLEAN
 	IS
+		v_twosum_element_index	INTEGER;
 	BEGIN
-		DBMS_OUTPUT.PUT_LINE('has two sum');
+		sort(p_nums, p_nums.FIRST, p_nums.LAST);		
+		v_twosum_element_index := find_twosum_element(p_nums, p_sum);
+		IF v_twosum_element_index <> NULL THEN
+			RETURN TRUE;
+		ELSE
+			RETURN FALSE;
+		END IF;
 	END;
 
 
 	PROCEDURE sort
 	(
-		p_nums	IN t_nums,
-		p_begin	IN INTEGER,
-		p_end	IN INTEGER
+		p_nums	IN OUT	t_nums,
+		p_begin	IN	INTEGER,
+		p_end	IN	INTEGER
 	)
 	IS
+		v_pivot		INTEGER;
+		v_left_end	INTEGER;
+		v_right_begin	INTEGER;
 	BEGIN
-		DBMS_OUTPUT.PUT_LINE('sort');
+		IF p_begin IS NULL
+		  OR p_end IS NULL THEN
+			RETURN;
+		END IF;
+
+		IF p_begin < p_end THEN
+			v_pivot := partition(p_nums, p_begin, p_end);
+
+			v_left_end := p_nums.PRIOR(v_pivot);
+			v_right_begin := p_nums.NEXT(v_pivot);
+
+			IF v_left_end IS NOT NULL
+			  AND p_begin < v_left_end THEN
+				sort(p_nums, p_begin, v_left_end);
+			END IF;
+
+			IF v_right_begin IS NOT NULL
+			  AND v_right_begin < p_end THEN
+				sort(p_nums, v_right_begin, p_end);
+			END IF;
+		END IF;
 	END;
 
 
-	PROCEDURE partition
+	FUNCTION partition
 	(
-		p_nums	IN t_nums,
-		p_begin	IN INTEGER,
-		p_end	IN INTEGER
+		p_nums	IN OUT	t_nums,
+		p_begin	IN	INTEGER,
+		p_end	IN	INTEGER
+	) RETURN INTEGER
+	IS
+		v_left_ptr	INTEGER := p_begin;
+		v_right_ptr	INTEGER := p_nums.PRIOR(p_end);
+		v_pivot_val	INTEGER := p_nums(p_end);
+	BEGIN
+		IF v_right_ptr IS NULL THEN
+			RETURN p_begin;
+		END IF;
+		
+		WHILE v_left_ptr IS NOT NULL
+		  AND v_right_ptr IS NOT NULL LOOP
+
+			WHILE v_left_ptr IS NOT NULL
+			  AND v_left_ptr <= v_right_ptr
+			  AND p_nums(v_left_ptr) <= v_pivot_val LOOP
+				v_left_ptr := p_nums.NEXT(v_left_ptr);
+			END LOOP;
+
+			WHILE v_right_ptr IS NOT NULL
+			  AND v_left_ptr IS NOT NULL
+			  AND v_left_ptr <= v_right_ptr
+			  AND p_nums(v_right_ptr) >= v_pivot_val LOOP
+				v_right_ptr := p_nums.PRIOR(v_right_ptr);
+			END LOOP;
+
+			EXIT WHEN v_left_ptr IS NULL
+			  OR v_right_ptr IS NULL
+			  OR v_left_ptr > v_right_ptr;
+
+			swap(p_nums, v_left_ptr, v_right_ptr);
+			v_left_ptr := p_nums.NEXT(v_left_ptr);
+			v_right_ptr := p_nums.PRIOR(v_right_ptr);
+		END LOOP;
+
+		IF v_left_ptr IS NULL THEN
+			RETURN p_end;
+		ELSE
+			swap(p_nums, p_end, v_left_ptr);
+			RETURN v_left_ptr;
+		END IF;
+	END;
+
+
+	PROCEDURE swap
+	(
+		p_nums	IN OUT	t_nums,
+		p_idx_a	IN	INTEGER,
+		p_idx_b	IN	INTEGER
 	)
 	IS
+		temp INTEGER;
 	BEGIN
-		DBMS_OUTPUT.PUT_LINE('partition');
+		temp := p_nums(p_idx_a);
+		p_nums(p_idx_a) := p_nums(p_idx_b);
+		p_nums(p_idx_b) := temp;
 	END;
 
 	
-	PROCEDURE swap
-	(
-		p_nums	IN OUT t_nums,
-		p_val_a	IN INTEGER,
-		p_val_b	IN INTEGER
-	)
-	IS
-	BEGIN
-		DBMS_OUTPUT.PUT_LINE('swap');
-	END;
-
-
 	FUNCTION find_twosum_element
 	(
-		p_nums	IN t_nums,
-		p_sum	IN INTEGER
+		p_nums	IN OUT	t_nums,
+		p_sum	IN	INTEGER
 	) RETURN INTEGER
 	IS
 	BEGIN
@@ -120,11 +198,26 @@ IS
 	BEGIN
 		DBMS_OUTPUT.PUT_LINE('find');
 	END;
+
+
+	PROCEDURE print(nums IN t_nums)
+	IS
+	BEGIN
+		FOR i IN nums.FIRST..nums.LAST LOOP
+			DBMS_OUTPUT.PUT(nums(i));
+		END LOOP;
+		DBMS_OUTPUT.PUT_LINE('');
+	END;
 END;
 /
 
 DECLARE
+	nums twosum.t_nums;
 BEGIN
 	DBMS_OUTPUT.PUT_LINE('Two Sum');
+	nums := twosum.t_nums(3, 6, 1, 2, 0, 7);
+	twosum.print(nums);
+	twosum.sort(nums, nums.FIRST, nums.LAST);
+	twosum.print(nums);
 END;
 /
